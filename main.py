ef8e25c9466fe909821d1a54c327b9d7d5a290c2
soplainly <evelastwaffe@gmail.com>
2023-03-08 19:40:05 +0300

    lab

---
+++ main.py
@@ -0,0 +1,21 @@
+
+str = str()
+
+while True:
+    n = input()
+    if n == "stop":
+        print(str,"Конец программы")
+        break
+    else:
+        str = str + " " + n
+        print(str)
+def prog1 ():
+    n = input()
+    while True:
+        if "ф" in n:
+            print("Ого! Это редкое слово")
+        else:
+            print("Не редкое")
+            break
+def prog2()           
+prog1()
